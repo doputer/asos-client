ab754d9a83b6bb93072475f0ad555fd9e20024a6
KIMDOHYEON <swputer@gmail.com>
2021-12-05 18:41:04 +0900

    style: 폴더 구조 분리

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './Components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
