818b7b8becf9729f5457b2a2f2c7637214e7a18e
KIMDOHYEON <swputer@gmail.com>
2021-12-15 18:43:30 +0900

    feat: 마이페이지 좌석 위치 생성

--- src/Components/MyPageDescription/index.tsx
+++ src/Components/MyPageDescription/index.tsx
@@ -1,19 +1,46 @@
 import { Avatar, Button, Card, Divider, Empty, Form, Input, Space } from 'antd';
+import { getSearchedReservation } from 'Apis/reservationApi';
 import { getUser } from 'Apis/userApi';
+import { SearchBoardCover } from 'Components/SearchBoardCover';
 import { Spinner } from 'Components/Spin';
 import useAsync from 'Hooks/useAsync';
-import { useEffect } from 'react';
+import { IReservation } from 'Interfaces/IReservation';
+import { useEffect, useState } from 'react';
 
 export const MyPageDescription = ({ userId }: { userId: number }) => {
-  const { loading, data: user, execute } = useAsync(getUser);
+  const { loading, data: user, execute: refetchUser } = useAsync(getUser);
+  const { data: reservations, execute: refetchReservations } = useAsync(
+    getSearchedReservation,
+  );
+
+  const [seatReservation, setSeatReservation] = useState<IReservation>();
+  const [roomReservation, setRoomReservation] = useState<IReservation>();
+  const [seatToggle, setSeatToggle] = useState(false);
+  const [roomToggle, setRoomToggle] = useState(false);
 
   useEffect(() => {
-    execute(userId);
+    refetchUser(userId);
+    refetchReservations({ userId });
   }, []);
+
+  useEffect(() => {
+    if (reservations) {
+      setSeatReservation(
+        reservations.find(
+          (reservation: IReservation) => reservation.seat !== null,
+        ),
+      );
+      setRoomReservation(
+        reservations.find(
+          (reservation: IReservation) => reservation.room !== null,
+        ),
+      );
+    }
+  }, [reservations]);
 
   return (
     <>
-      {loading && !user !== null ? (
+      {loading && !user && !reservations !== null ? (
         <Spinner />
       ) : (
         <div
@@ -28,6 +55,7 @@ export const MyPageDescription = ({ userId }: { userId: number }) => {
           <div
             style={{
               flex: '1 0 auto',
+              minWidth: '360px',
             }}
           >
             <Card
@@ -102,30 +130,81 @@ export const MyPageDescription = ({ userId }: { userId: number }) => {
                 <Form.Item label="직책">
                   <Input defaultValue={user?.position} disabled />
                 </Form.Item>
+                {seatReservation && (
+                  <Form.Item label="좌석 위치">
+                    {`${seatReservation?.seat.floor.name} - ${seatReservation?.seat.name}`}
+                    <Button
+                      type="ghost"
+                      shape="round"
+                      style={{
+                        marginLeft: '10px',
+                      }}
+                      onClick={() => setSeatToggle(!seatToggle)}
+                    >
+                      위치 보기
+                    </Button>
+                  </Form.Item>
+                )}
+                {roomReservation && (
+                  <Form.Item label="좌석 위치">
+                    {`${roomReservation.room.floor.name} - ${roomReservation.room.name}`}
+                    <Button
+                      type="ghost"
+                      shape="round"
+                      size="small"
+                      style={{
+                        marginLeft: '10px',
+                      }}
+                    >
+                      위치 보기
+                    </Button>
+                  </Form.Item>
+                )}
               </Form>
             </Card>
           </div>
           <div
-            className="flex-center"
             style={{
               flex: 7,
               border: '1px dashed #c2c2c2',
+              overflow: 'auto',
+              height: '100%',
             }}
           >
-            <Empty
-              description={
-                <>
-                  <div>
-                    <span style={{ color: '#4895ef' }}>좌석</span> 혹은{' '}
-                    <span style={{ color: '#4895ef' }}>회의실</span>의 위치가
-                  </div>
-                  <div>이곳에 나타납니다.</div>
-                </>
-              }
-              style={{
-                margin: '36px',
-              }}
-            />
+            {seatToggle ? (
+              <div
+                style={{
+                  minHeight: '360px',
+                }}
+              >
+                {seatReservation && (
+                  <SearchBoardCover reservation={seatReservation} />
+                )}
+              </div>
+            ) : (
+              <div
+                className="flex-center"
+                style={{
+                  height: '100%',
+                }}
+              >
+                <Empty
+                  description={
+                    <>
+                      <div>
+                        <span style={{ color: '#4895ef' }}>좌석</span> 혹은{' '}
+                        <span style={{ color: '#4895ef' }}>회의실</span>의
+                        위치가
+                      </div>
+                      <div>이곳에 나타납니다.</div>
+                    </>
+                  }
+                  style={{
+                    margin: '36px',
+                  }}
+                />
+              </div>
+            )}
           </div>
         </div>
       )}
